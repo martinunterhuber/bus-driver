51ecb5e6829836784747f49a269f0cc7ce1ff74d
Martin Unterhuber <m.unterhuber99@gmail.com>
2021-07-09 18:55:00 +0200

    Added variable player count

---
+++ app/src/main/java/at/unterhuber/bus_driver/adapters/PlayerAdapter.kt
@@ -0,0 +1,48 @@
+package at.unterhuber.bus_driver.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.core.widget.doOnTextChanged
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bus_driver.R
+
+class PlayerAdapter(private val context: Context) : RecyclerView.Adapter<PlayerAdapter.ViewHolder>() {
+    var players = ArrayList<String>(listOf(""))
+    private var count = 1
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.new_player_item, parent, false)
+        val holder =  ViewHolder(view)
+        holder.setIsRecyclable(false)
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.playerName.hint = context.getString(R.string.player_n, position + 1)
+        holder.playerName.setText(players[position])
+        holder.playerName.doOnTextChanged { text, start, before, count -> players[position] = holder.playerName.text.toString() }
+    }
+
+    override fun getItemCount(): Int {
+        return count
+    }
+
+    fun addPlayer() {
+        players.add("")
+        count++
+        notifyItemInserted(count - 1)
+    }
+
+    override fun onViewDetachedFromWindow(holder: ViewHolder) {
+        super.onViewDetachedFromWindow(holder)
+        holder.playerName.doOnTextChanged{ _, _, _, _ -> {} }
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val playerName: EditText = view.findViewById(R.id.player_name)
+    }
+}
--- app/src/main/java/at/unterhuber/bus_driver/ui/MainActivity.kt
+++ app/src/main/java/at/unterhuber/bus_driver/ui/MainActivity.kt
@@ -2,31 +2,43 @@ package at.unterhuber.bus_driver.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Spinner
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import at.unterhuber.bus_driver.adapters.PlayerAdapter
 
 import com.example.bus_driver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: PlayerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.startButton.setOnClickListener{
-            val players = ArrayList<String>()
-            players.add(binding.player1.text.toString())
-            if (binding.player2.text.isNotBlank())
-                players.add(binding.player2.text.toString())
-            if (binding.player3.text.isNotBlank())
-                players.add(binding.player3.text.toString())
-            if (binding.player4.text.isNotBlank())
-                players.add(binding.player4.text.toString())
-            val intent = Intent(this, ChoiceActivity::class.java)
-            intent.putExtra("PLAYERS", players)
-            startActivity(intent)
+        adapter = PlayerAdapter(this)
+        binding.players.adapter = adapter
+        binding.players.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.addPlayer.setOnClickListener{adapter.addPlayer()}
+
+        binding.startButton.setOnClickListener{startGame()}
+    }
+
+    private fun startGame() {
+        var players = (binding.players.adapter!! as PlayerAdapter).players
+        players = players.filter { it.isNotBlank() } as ArrayList<String>
+        if (players.isEmpty()) {
+            players.add("")
         }
+        val intent = Intent(this, ChoiceActivity::class.java)
+        intent.putExtra("PLAYERS", players)
+        startActivity(intent)
     }
 }
